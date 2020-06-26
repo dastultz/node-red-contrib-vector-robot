940695f5132e39e2a6c0e8504520e2a4ff60434e
Daryl Stultz <kungfumachinist@gmail.com>
2020-06-25 20:31:21 -0400

    add code to update official SDK assets

--- update_proto.sh
+++ update_proto.sh
@@ -2,6 +2,9 @@
 
 # A script to pull and update official SDK assets
 
+# TODO this isn't quite done yet, it's meant to pull or update a local copy of the official
+# SDK and copy some files into this project
+
 DEST_DIR=lib/protobufs/anki_vector/messaging
 mkdir -p ${DEST_DIR}
 rm -f ${DEST_DIR}/*.proto
@@ -10,6 +13,8 @@ rm -f ${DEST_DIR}/*.proto
 # then git fetch/pull
 # else
 # git clone https://github.com/anki/vector-python-sdk.git
-
+cd vector-python-sdk
+git pull
+cd ../
 cp vector-python-sdk/anki_vector/messaging/*.proto ${DEST_DIR}
 
